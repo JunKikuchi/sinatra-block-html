403288d0bbd507ab3c3f3e0cba7c99187a0357d5
菊池 淳 <kikuchi@bonnou.com>
2009-06-02 00:18:46 +0900

    first commit

---
+++ lib/sinatra/block-html.rb
@@ -0,0 +1,27 @@
+require 'sinatra/base'
+require 'block-html'
+
+module Sinatra
+  module BlockHTML
+    def bhtml(template=nil, options={}, locals={}, &block)
+      require_warn('BlockHTML') unless defined?(::BlockHTML)
+
+      options, template = template, nil if template.is_a?(Hash)
+      template = lambda { block } if template.nil?
+      render :bhtml, template, options, locals
+    end
+
+  protected
+    def render_bhtml(template, data, options, locals, &block)
+      if data.respond_to?(:to_str)
+        ::BlockHTML.new(self) do
+          instance_eval(data)
+        end
+      elsif data.kind_of?(Proc)
+        ::BlockHTML.new(self, &data)
+      end
+    end
+  end
+
+  helpers BlockHTML
+end
